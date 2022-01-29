dead712456d58d87e1530f5db9408ca71686078a
Devessier <baptiste@devessier.fr>
2022-01-29 10:27:23 +0100

    refactor: put submission handling in child component

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import CheckboxList from "../components/CheckboxList";
 import { todosMachine } from "../machines/todosMachine";
 
 interface CreateTodoFormProps {
-  handleSaveTodo: (event: FormEvent<HTMLFormElement>) => void;
+  handleSaveTodo: (newTodo: string) => void;
   handleCloseTodoCreation: () => void;
 }
 
@@ -15,6 +15,29 @@ function CreateTodoForm({
   handleSaveTodo,
   handleCloseTodoCreation,
 }: CreateTodoFormProps) {
+  function handleFormSubmit({ target }: FormEvent<HTMLFormElement>) {
+    if (target === null) {
+      return;
+    }
+
+    const formData = new FormData(target as HTMLFormElement);
+    const newTodo = formData.get("new-todo");
+    if (typeof newTodo !== "string") {
+      return;
+    }
+
+    const sanitizedNewTodo = sanitizeNewTodo(newTodo);
+    if (sanitizedNewTodo.length === 0) {
+      return;
+    }
+
+    handleSaveTodo(newTodo);
+  }
+
+  function sanitizeNewTodo(todo: string): string {
+    return todo.trim();
+  }
+
   return (
     <div className="bg-white shadow sm:rounded-lg">
       <div className="px-4 py-5 sm:p-6">
@@ -27,7 +50,7 @@ function CreateTodoForm({
           onSubmit={(event) => {
             event.preventDefault();
 
-            handleSaveTodo(event);
+            handleFormSubmit(event);
           }}
         >
           <div className="w-full sm:max-w-xs">
@@ -90,26 +113,7 @@ const Home: NextPage = () => {
     });
   }
 
-  function sanitizeNewTodo(todo: string): string {
-    return todo.trim();
-  }
-
-  function handleSaveTodo({ target }: FormEvent<HTMLFormElement>) {
-    if (target === null) {
-      return;
-    }
-
-    const formData = new FormData(target as HTMLFormElement);
-    const newTodo = formData.get("new-todo");
-    if (typeof newTodo !== "string") {
-      return;
-    }
-
-    const sanitizedNewTodo = sanitizeNewTodo(newTodo);
-    if (sanitizedNewTodo.length === 0) {
-      return;
-    }
-
+  function handleSaveTodo(newTodo: string) {
     send({
       type: "SAVE_TODO",
       todo: newTodo,
